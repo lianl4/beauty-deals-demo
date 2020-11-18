7ba27d60fbca5f0d01d6aee5202f1f4d44334431
dorothy0225 <jiyuxiaochuan@163.com>
2020-11-17 23:04:53 -0800

    [Frontend]Recommited Upload function

--- frontend/src/Pages/searchResult.js
+++ frontend/src/Pages/searchResult.js
@@ -10,8 +10,10 @@ import {
     Box,
     Grommet, Main, Nav, Tabs, Tab, TextInput,
     ResponsiveContext,
+    Anchor,
+    Header,
   } from "grommet";
-import { AddCircle, Favorite } from "grommet-icons";
+import { AddCircle, Favorite,Login,Home } from "grommet-icons";
 import { useHistory } from 'react-router-dom'
 
 
@@ -40,17 +42,21 @@ const SearchResult = () => {
   return (
     <Grommet full theme={theme}>
       <Main fill="vertical" flex="grow" overflow="auto">
-        <Nav align="baseline" flex={false} direction="row" justify="between" gap="xlarge" pad="small" background={{"color":"text-strong"}}>
-          <Tabs justify="start">
-            <Tab title="Home" />
-            <Tab title="Favorites" />
-            <Tab title="Sign In/Up" />
-          </Tabs>
+      <Header align="center" direction="row" justify="between" gap="small" color = "text-strong"  background={{"color":"text-strong"}} pad={{"horizontal":"xsmall","vertical":"medium"}} flex="grow" fill="horizontal" overflow="auto">
+          <Nav align="center" flex={false} justify="end">
+          <Box align="end" justify="center" direction="row" gap="medium" alignSelf="end">
+            <Anchor label="Home" icon={<Home />} size="medium"
+            onClick={()=> history.push("/") } />
+            <Anchor label="Favorite" icon={<Favorite />} size="medium" />
+            <Anchor label="Sign In/Up" icon={<Login />} size="medium" 
+            onClick={()=> history.push("/signup") }/>
+          </Box>
+          </Nav>
           <Box align="baseline" justify="between" direction="row" gap="xsmall">
             <TextInput placeholder="Find an Item?" />
             <Button label="Search" onClick={()=> history.push("/search-result")}/>
           </Box>
-        </Nav>
+        </Header>
     <Box align="center" justify="start" pad="xlarge" height="large">
       <Table size ="large" >
         <TableHeader>
